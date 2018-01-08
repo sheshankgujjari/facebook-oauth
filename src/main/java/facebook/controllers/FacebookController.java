de8ca242e608aacb77b561795d9620b5c243d34f
Sheshank Gujjari <sheshankgujjari@hotmail.com>
2018-01-07 23:21:37 -0500

    Facebook get access token using appId and appSappSecret using javafx

---
+++ src/main/java/facebook/controllers/FacebookController.java
@@ -0,0 +1,31 @@
+package facebook.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import facebook.model.Greeting;
+import facebook.javafx.LoginJavaFXHelper;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FacebookController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+    @RequestMapping(value = "/getouthtoken/{appId}/{appSecret}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String getOauthToken(@PathVariable final String appId, @PathVariable final String appSecret) {
+        String[] args = {appId, appSecret};
+        String[] result = LoginJavaFXHelper.getAccessToken(args);
+        return result[0];
+    }
+}
---
+++ src/main/java/facebook/javafx/Browser.java
@@ -0,0 +1,86 @@
+
+package facebook.javafx;
+
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Version;
+import com.restfb.scope.ScopeBuilder;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.layout.Region;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+public class Browser extends Region {
+
+  public static final String SUCCESS_URL = "https://www.facebook.com/connect/login_success.html";
+  final WebView browser = new WebView();
+  final WebEngine webEngine = browser.getEngine();
+  private String code;
+
+  private final String appId;
+
+  private final String appSecret;
+
+  public Browser(String appId, String appSecret) {
+    this.appId = appId;
+    this.appSecret = appSecret;
+    // add the web view to the scene
+    getChildren().add(browser);
+  }
+
+  public String[] showLogin() {
+    DefaultFacebookClient facebookClient = new DefaultFacebookClient(Version.LATEST);
+    ScopeBuilder scopes = new ScopeBuilder();
+    String loadUrl = facebookClient.getLoginDialogUrl(appId, SUCCESS_URL, scopes);
+    final String[] result = {};
+    webEngine.load(loadUrl + "&display=popup&response_type=code");
+    webEngine.getLoadWorker().stateProperty().addListener(
+      (ObservableValue<? extends Worker.State> observable, Worker.State oldValue, Worker.State newValue) -> {
+        if (newValue != Worker.State.SUCCEEDED) {
+          return;
+        }
+
+        String myUrl = webEngine.getLocation();
+
+        if ("https://www.facebook.com/dialog/close".equals(myUrl)) {
+          System.out.println("dialog closed");
+          System.exit(0);
+        }
+
+        if (myUrl.startsWith(SUCCESS_URL)) {
+          System.out.println(myUrl);
+          int pos = myUrl.indexOf("code=");
+          code = myUrl.substring(pos + "code=".length());
+          FacebookClient.AccessToken token = facebookClient.obtainUserAccessToken(appId,
+                  appSecret, SUCCESS_URL, code);
+          System.out.println("Accesstoken: " + token.getAccessToken());
+          System.out.println("Expires: " + token.getExpires());
+          result[0] = token.getAccessToken();
+          result[1] = token.getExpires().toString();
+        }
+
+      });
+    return result;
+  }
+
+  @Override
+  protected void layoutChildren() {
+    double w = getWidth();
+    double h = getHeight();
+    layoutInArea(browser, 0, 0, w, h, 0, HPos.CENTER, VPos.CENTER);
+  }
+
+  @Override
+  protected double computePrefWidth(double height) {
+    return 900;
+  }
+
+  @Override
+  protected double computePrefHeight(double width) {
+    return 600;
+  }
+
+}
